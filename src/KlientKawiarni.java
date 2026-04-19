29d6e6646c4ad6063f3706b102d256c9cdb31290
plagye <spotifysess@gmail.com>
2026-04-19 17:50:05 +0200

    Dodanie klasy KlientKawiarni

---
+++ src/KlientKawiarni.java
@@ -0,0 +1,45 @@
+public class KlientKawiarni {
+    private int idKlienta;
+    private String imie;
+    private String nazwisko;
+    private String email;
+
+    private static int liczbaKlientow=0;
+
+    public static int  getLiczbaKlientow() {
+        return KlientKawiarni.liczbaKlientow;
+    }
+
+    public KlientKawiarni(int idKlienta, String imie, String nazwisko, String email) {
+        this.idKlienta = idKlienta;
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "KlientWypozyczalni{" +
+                "idKlienta=" + idKlienta +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof KlientKawiarni that)) return false;
+        return email == that.email;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(email);
+    }
+
+    public void wypiszDane() {
+        System.out.println("Imie: " + imie);
+        System.out.println("Nazwisko: " + nazwisko);
+        System.out.println("Email: " + email);
+        System.out.println("ID Klienta: " + idKlienta);
+    }
+}
+
